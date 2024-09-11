039c598dbbaeb0b40829a6fd8c94f1e6e05c0ceb
chuckfinca <chuckfinca@gmail.com>
2024-09-11 16:35:12 -0700

    Adds missing logger

---
+++ evaluate/logging/logger.py
@@ -0,0 +1,20 @@
+import logging
+
+class SingletonLogger:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(SingletonLogger, cls).__new__(cls)
+            cls._instance.log = logging.getLogger(__name__)
+            cls._instance.log.setLevel(logging.INFO)
+            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            handler = logging.StreamHandler()
+            handler.setFormatter(formatter)
+            cls._instance.log.addHandler(handler)
+        return cls._instance
+
+    def set_level(self, level):
+        self.log.setLevel(level)
+
+logger = SingletonLogger()
